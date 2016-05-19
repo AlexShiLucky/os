07bacfac47242c2bb9639c1675773e304243c831
Asim <asim@chuhnk.me>
2016-05-19 23:00:21 +0100

    Update kv implementation

--- kv/kv.go
+++ kv/kv.go
@@ -6,7 +6,12 @@ import (
 
 	"github.com/micro/cli"
 	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/server"
 	"github.com/micro/go-platform/kv"
+	"github.com/micro/kv-srv/handler"
+	proto "github.com/micro/kv-srv/proto/store"
+
+	"golang.org/x/net/context"
 )
 
 func srv(ctx *cli.Context) {
@@ -34,6 +39,15 @@ func srv(ctx *cli.Context) {
 
 	keyval := kv.NewKV(opts...)
 	defer keyval.Close()
+
+	service.Server().Init(server.WrapHandler(func(fn server.HandlerFunc) server.HandlerFunc {
+		return func(ctx context.Context, req server.Request, rsp interface{}) error {
+			ctx = kv.NewContext(ctx, keyval)
+			return fn(ctx, req, rsp)
+		}
+	}))
+
+	proto.RegisterStoreHandler(service.Server(), new(handler.Store))
 
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
